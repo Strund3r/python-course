6e8d8a5b4b07db10935b2900c4f52e6d8a65ddc3
Strund3r <franzwagner.str@gmail.com>
2017-06-16 07:41:09 -0300

    Add Lista 2

---
+++ Part 1/Week 3/Lista 2/fizz.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um valor: "))
+
+if n % 3 == 0:
+	print("Fizz")
+else:
+	print(n)
---
+++ Part 1/Week 3/Lista 2/paridade.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um valor: "))
+
+if n % 2 == 0:
+	print("O valor", n, "é par.")
+else:
+	print("O valor", n, "é impar.")
